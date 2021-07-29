37bf5c62b5dd8f411bee2ec791686a68e8c2eb01
Amanda Miquilini <amiquilini@id.uff.br>
2021-07-29 15:56:57 -0300

    Implementando validacao de devedores para abrir novas contas

--- Forms/FormCadastroNovaConta.cs
+++ Forms/FormCadastroNovaConta.cs
@@ -1,37 +1,51 @@
 ﻿using CaixaEletronico.Contas;
+using CaixaEletronico.Servicos;
 using CaixaEletronico.Usuarios;
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace CaixaEletronico {
     public partial class FormCadastroNovaConta : Form {
+
+        private ICollection<string> devedores;
+
         private FormCaixaEletronico formPrincipal;
+
         public FormCadastroNovaConta(FormCaixaEletronico formPrincipal) {
             this.formPrincipal = formPrincipal;
             InitializeComponent();
+
+            GeradorDeDevedores g = new GeradorDeDevedores();
+            devedores = g.GerarListaNomes();
         }
 
         private void btnCadastrar_Click(object sender, EventArgs e) {
             if (ValidarCampos()) {
-                if(comboTipoConta.Text == "Corrente") {
-                    Conta cc = new ContaCorrente(new Cliente(txtTitular.Text), 0);
-                    this.formPrincipal.AdicionaNovaConta(cc);
-                }
-                else if (comboTipoConta.Text == "Poupança") {
-                    Conta cc = new ContaPoupanca(new Cliente(txtTitular.Text), 0);
-                    this.formPrincipal.AdicionaNovaConta(cc);
-                }
-                else if (comboTipoConta.Text == "Estudante") {
-                    Conta cc = new ContaEstudante(new Cliente(txtTitular.Text), 0);
-                    this.formPrincipal.AdicionaNovaConta(cc);
-                }
-                else if (comboTipoConta.Text == "Investimento")
+                if (!TitularEhDevedor(txtTitular.Text))
                 {
-                    Conta cc = new ContaInvestimento(new Cliente(txtTitular.Text), 0);
-                    this.formPrincipal.AdicionaNovaConta(cc);
+                    if (comboTipoConta.Text == "Corrente")
+                    {
+                        Conta cc = new ContaCorrente(new Cliente(txtTitular.Text), 0);
+                        this.formPrincipal.AdicionaNovaConta(cc);
+                    }
+                    else if (comboTipoConta.Text == "Poupança")
+                    {
+                        Conta cc = new ContaPoupanca(new Cliente(txtTitular.Text), 0);
+                        this.formPrincipal.AdicionaNovaConta(cc);
+                    }
+                    else if (comboTipoConta.Text == "Estudante")
+                    {
+                        Conta cc = new ContaEstudante(new Cliente(txtTitular.Text), 0);
+                        this.formPrincipal.AdicionaNovaConta(cc);
+                    }
+                    else if (comboTipoConta.Text == "Investimento")
+                    {
+                        Conta cc = new ContaInvestimento(new Cliente(txtTitular.Text), 0);
+                        this.formPrincipal.AdicionaNovaConta(cc);
+                    }
+                    this.Close();
                 }
-
-                this.Close();
             }
         }
 
@@ -46,7 +60,20 @@ namespace CaixaEletronico {
                 MessageBox.Show("Selecione um tipo de conta!");
                 retornoValidacao = false;
             }
+            
             return retornoValidacao;
+        }
+
+        private bool TitularEhDevedor(string titular)
+        {
+            bool ehDevedor = devedores.Contains(titular);
+
+            if (ehDevedor)
+            {
+                MessageBox.Show("Titular é um devedor!");
+            }
+
+            return ehDevedor;
         }
 
         private void FormCadastroNovaConta_Load(object sender, EventArgs e) {
---
+++ Servicos/GeradorDeDevedores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaixaEletronico.Servicos
+{
+    class GeradorDeDevedores
+    {
+        public HashSet<string> GerarListaNomes()
+        {
+            var nomes = new HashSet<string>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                nomes.Add("Devedor " + i);
+            }
+            return nomes;
+        }
+    }
+}
